ccbcffb09f98d4a532a0a035f6b054620979b7d9
euheber <euheber1@gmail.com>
2022-06-15 22:37:35 -0300

    desktop version

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', ()=> { 
+     let height = window.scrollY
+
+     if(height >= 700){ 
+        console.log(window.scrollY)
+     } else { 
+        console.log(window.scrollY)
+     }
+})
